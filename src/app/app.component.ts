c5cc79838e1c4718f2ec0c55f1e67cbcd0d2d71e
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-09-17 18:20:58 -0500

    1st ver wrap up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hi! I\'m interested in materializing brilliant ideas into tangible products.';
+  alt = 'In other words, I\'m a front-end engineer.';
+
+  displayText = this.title;
+
+  onMouseOver() {
+
+  }
 }
